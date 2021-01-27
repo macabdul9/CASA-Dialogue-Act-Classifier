0a8f9ff111a3e4a8d22e4f9ec184632c5ddbebba
macabdul9 <abdulwaheed1513@gmail.com>
2021-01-27 17:33:07 +0530

    fix #5

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -6,14 +6,16 @@ class DADataset(Dataset):
     __label_dict = dict()
     
     def __init__(self, tokenizer, data, text_field = "clean_text", label_field="act_label_1", max_len=512):
+        
         self.text = list(data[text_field]) #data['train'][text_field]
-        self.label = list(data[label_field]) #['train'][label_field]
+        self.acts = list(data[label_field]) #['train'][label_field]
         self.tokenizer = tokenizer
         self.max_len = max_len
         
         
         # build/update the label dictionary 
-        classes = set(self.label)
+        classes = sorted(set(self.acts))
+        
         for cls in classes:
             if cls not in DADataset.__label_dict.keys():
                 DADataset.__label_dict[cls]=len(DADataset.__label_dict.keys())
@@ -27,8 +29,8 @@ class DADataset(Dataset):
     def __getitem__(self, index):
         
         text = self.text[index]
-        label = self.label[index]
-        target = DADataset.__label_dict[label]
+        act = self.acts[index]
+        label = DADataset.__label_dict[act]
         
         input_encoding = self.tokenizer.encode_plus(
             text=text,
@@ -46,6 +48,6 @@ class DADataset(Dataset):
             "input_ids":input_encoding['input_ids'].squeeze(),
             "attention_mask":input_encoding['attention_mask'].squeeze(),
             "seq_len":seq_len,
-            "target":label,
-            "label":torch.tensor([target], dtype=torch.long),
+            "act":act,
+            "label":torch.tensor([label], dtype=torch.long),
         }
